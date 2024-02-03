32356d01b980feb3b6032cbfebfdb97ef42bd803
RockieKelz <racquy@msn.com>
2024-02-02 21:46:27 -0800

    Adjusted Recently Played view to show song information within a card and made text scroll if the data would create more than one line.

--- pages/Home.js
+++ pages/Home.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from "react";
 import { Image, Pressable, SafeAreaView,Text, StyleSheet, View } from 'react-native';
 import { ScrollView } from "react-native-gesture-handler";
+import TextTicker from 'react-native-text-ticker';
 import { fetchPlaylists, fetchProfile, fetchRecentlyPlayed, getRecentlyPlayed, logOut } from "../utils/spotify";
 import { SideBar } from "../components/SideBar";
 import { LinearGradient } from 'expo-linear-gradient'
@@ -61,40 +62,95 @@ This use effect will be deleted once the Recently Played section is completed */
                 </Text>
                 {/*display recently played songs if there are any*/}
                 <Text style={styles.title}>
-                  Recently Played Songs
-                  </Text>
-                <View>{recentPlays && recentPlays.recentlyPlayed.items?.map((item, index) => (
-                  <View key={index} style={{flexDirection: 'row'}}>
-                    <View style={{backgroundColor: 'cyan', flexDirection: 'column'}}>
-                      {/*song's information*/}
-                      <Text>Song: {item.track.name}</Text>
-                      <Text>Artist: <></>
-                        {item.track.artists?.map((it, index) => (
-                          <View key={index}>
-                          <Text>{it.name} </Text>
-                          </View>))}
-                        </Text>
-                      <Text>Album: {item.track.album.name}</Text>
+                   Recently Played Songs
+                </Text>
+                <View style={styles.cardContainer}>
+                  {/*create card for each song*/}
+                {recentPlays && recentPlays.recentlyPlayed.items?.map((item, index) => (
+                  <View 
+                    key ={index}
+                    style={styles.card}>
+                      {/*song's information:
+                        SONG NAME as card's header*/}
+                      <View style={{ width: 145, height: 30 }}>
+                        <TextTicker /* Looping scroll for track name */
+                          style={styles.primaryName}
+                          duration={7500}
+                          animationType={'auto'}
+                          marqueeDelay={1250}
+                          repeatSpacer={65}>
+                        {item.track.name}
+                        </TextTicker>
+                      </View>
+                      {/* =========================================================
+                     
+                              TO DO: IF CHECK FOR WHEN THERE IS NO ALBUM IMAGES
+                     
+                     =============================================================*/}
                       {item.track.album.images[0] && (<>
-                        <Image source={{uri: item.track.album.images[0].url}}
+                      <Image 
+                        source={{uri: item.track.album.images[0].url}}
                         style={styles.albumImage} />
                         </>)}
-                    </View>
+                                              
+                      {/*Album and Artist info as subheaders*/}
+                      <View style ={{width:145, flexDirection: 'row'}}>
+                        <Text style={styles.labelName}>Artist: </Text>
+                        <View style ={{width:105}}>
+                          <TextTicker /* bouncing horizontal scroll for artist(s) */
+                            style = {styles.subName}
+                            duration={4500}
+                            animationType={'bounce'}
+                            marqueeDelay={1250}
+                            repeatSpacer={65}>
+                              {/*Check if there's multiple artists and separate their names*/}
+                              {item.track.artists.length > 1 ? (
+                              item.track.artists.map((artist, index) => (
+                                <Text key={index}>
+                                  {artist.name} 
+                                  {index == item.track.artists.length - 1 ? (
+                                    <Text> {" "} </Text>
+                                    ) : (
+                                    <Text>, </Text>
+                                  )}
+                                </Text>
+                                ))
+                              ) : (
+                              <Text>
+                                {item.track.artists[0].name}
+                              </Text>)}
+                          </TextTicker>
+                        </View>
+                      </View>
+                      <View style ={{width:146, flexDirection: 'row'}}>
+                        <Text style={styles.labelName}>Album: </Text>
+                        <View style ={{width:100}}>
+                          <TextTicker  /* bouncing horizontal scroll for album */
+                            style = {styles.subName}
+                            duration={7700}
+                            animationType={'bounce'}
+                            marqueeDelay={1250}
+                            repeatSpacer={65}>
+                              {item.track.album.name}
+                          </TextTicker>
+                        </View>
+                      </View>
                   </View>
-           ))}</View>
-                {/*Temporary logout button to test authorization code*/}
-                <Pressable 
-                  style={styles.btn} 
-                  onPress={logOut}>
-                    <Text style={styles.text}>Log Out</Text>
-                </Pressable>
+            ))}
             </View>
-          </ScrollView>
-        </LinearGradient>
-      </View>
-      <View style={styles.footerLine} />
+            {/*Temporary logout button to test authorization code*/}
+            <Pressable 
+              style={styles.btn} 
+              onPress={logOut}>
+                <Text style={styles.logoutText}>Log Out</Text>
+            </Pressable>
+          </View>
+        </ScrollView>
+      </LinearGradient>
+    </View>
+    <View style={styles.footerLine} />
     {/*Footer area to display music player*/}            
-    </SafeAreaView>
+  </SafeAreaView>
   )
 }
 
@@ -122,7 +178,7 @@ const styles = StyleSheet.create({
     fontFamily: "Arial",
     fontSize: 22
   },
-  /*logout button */
+  /*logout */
   btn: {
     width: 140,
     height: 40,
@@ -134,17 +190,61 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text: {
+  /*card like views*/
+  cardContainer: {
+    marginBottom: 16,
+    marginStart: 10, 
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    flexWrap: 'wrap',
+  },
+  card:{
+    width: '17%',
+    height: 245,
+    backgroundColor: 'rgba(0, 245, 255, 0.80)',
+    borderRadius: 2,
+    shadowColor: '#000', 
+    shadowOffset: { width: 2, height: 4 }, 
+    shadowOpacity: 0.2, 
+    shadowRadius: 2, 
+    elevation: 2, 
+    padding: 12,
+    margin: 10,
+    alignItems: 'center', 
+    justifyContent: 'space-between', 
+    minWidth: 165,
+  },
+  labelName: {
+    fontSize: 14,
+    color: '#666',
+  },
+  /*song data views*/
+  primaryName: {
+    fontSize: 15,
+    color: 'black',
+    fontWeight: 'bold',
+    marginBottom: 4,
+    textAlign: 'center'
+  },
+  albumImage: {
+    width: 150,
+    height: 150,
+    marginBottom: 4,
+   },
+   subName: {
+    fontSize: 14,
+    color: 'black',
+    fontWeight: '600',
+    marginBottom: 4,
+  },
+  /*addt'l sections*/
+   logoutText: {
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
     letterSpacing: 3,
   },
-  albumImage: {
-    width: 150,
-    height: 150,
-    marginBottom: 10,
-   },
   /*line that separates the bottom player from the upper components */
   footerLine:{
     borderBottomColor: "#7001b1",
